3f7d0205934795f2f946358973095f8c5ade7e83
SarahAmr-br <sab@blueribbon-inc.com>
2024-08-12 15:16:55 +0300

    Update flutter_flow_theme.dart

--- lib/flutter_flow/flutter_flow_theme.dart
+++ lib/flutter_flow/flutter_flow_theme.dart
@@ -146,7 +146,7 @@ class LightModeTheme extends FlutterFlowTheme {
   @Deprecated('Use tertiary instead')
   Color get tertiaryColor => tertiary;
 
-  late Color primary =  const Color(0xFF4B39EF);
+  late Color primary =  Color(0xFF4B39EF);
   late Color secondary = const Color(0xFF4B39EF);
   late Color tertiary = const Color(0xFFEE8B60);
   late Color alternate = const Color(0xFFF1F4F8);
