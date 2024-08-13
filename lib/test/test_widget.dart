44e9fb2cd635ac6f43734223577cfa05be5e6bc5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-13 10:38:42 +0000

    Updating to latest FlutterFlow output.

--- lib/test/test_widget.dart
+++ lib/test/test_widget.dart
@@ -131,6 +131,15 @@ class _TestWidgetState extends State<TestWidget> {
                         },
                       ),
                     ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset(
+                        'assets/images/download.jpeg',
+                        width: 300.0,
+                        height: 200.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ],
                 ),
               ),
